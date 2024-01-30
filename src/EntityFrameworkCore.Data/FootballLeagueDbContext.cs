3532cf3e4e103ef97dbfb9f471aba724fe23e62b
LairdLuca <luca.sanna.1503@gmail.com>
2024-01-30 18:17:11 +0100

    Add a Database Context

---
+++ src/EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Data
+{
+    public class FootballLeagueDbContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
