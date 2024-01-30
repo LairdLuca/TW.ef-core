ee091b5fd6dfca81e17daf5769f3bbb17704de83
LairdLuca <luca.sanna.1503@gmail.com>
2024-01-30 16:21:21 +0100

    Creating the Data Models with EF Core

---
+++ src/EntityFrameworkCore.Domain/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace EntityFrameworkCore.Domain
+{
+    public class Team : BaseDomainModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Coach Coach { get; set; }
+    }
+}
