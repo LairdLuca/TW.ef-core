c391a1ef4397cfabe1f3f40370b778dac06fde0a
LairdLuca <luca.sanna.1503@gmail.com>
2024-02-05 20:32:35 +0100

    Querying Basics

--- src/EntityFrameworkCore.Console/Program.cs
+++ src/EntityFrameworkCore.Console/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace EntityFrameworkCore.Console
+﻿using EntityFrameworkCore.Data;
+using EntityFrameworkCore.Domain;
+
+namespace EntityFrameworkCore.Console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, World!");
+            //first we need an instance of the context
+            var context = new FootballLeagueDbContext();
+
+            //select all the teams
+            //SELECT * FROM Teams
+            List<Team> teams = context.Teams.ToList();
+
+            foreach (Team team in teams)
+            {
+                System.Console.WriteLine(team.Name);
+            }
         }
     }
 }
