f0ee18181f15d63087aa449288d57b36f31dc98e
LairdLuca <luca.sanna.1503@gmail.com>
2024-02-08 17:33:47 +0100

    Querying for a Single Record

--- src/EntityFrameworkCore.Console/Program.cs
+++ src/EntityFrameworkCore.Console/Program.cs
@@ -1,17 +1,44 @@
 ﻿using EntityFrameworkCore.Data;
 using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace EntityFrameworkCore.Console
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            //first we need an instance of the context
-            var context = new FootballLeagueDbContext();
+        // First we need an instance of the context
+        private static FootballLeagueDbContext context = new FootballLeagueDbContext();
 
-            //select all the teams
-            //SELECT * FROM Teams
+        static async Task Main(string[] args)
+        {
+            // Select all teams
+            StampAllTeams();
+
+            // Selectiong a single record - First one in the list
+            //Team teamOne = await context.Teams.FirstAsync();
+            Team? teamOne = await context.Teams.FirstOrDefaultAsync();
+
+            // Selectiong a single record - First one in the list that meets a condition
+            //Team teamTwo = await context.Teams.FirstAsync(q => q.Id == 1);
+            Team? teamTwo = await context.Teams.FirstOrDefaultAsync(q => q.Id == 1);
+
+            // Selectiong a single record - Only one record should be returned
+            //Team teamThree = await context.Teams.SingleAsync(q => q.Id == 2);
+            Team? teamThree = await context.Teams.SingleOrDefaultAsync(q => q.Id == 2);
+
+            // Selectiong based on Id
+            Team? teamBasedOnId = await context.Teams.FindAsync(5);
+            if(teamBasedOnId != null)
+            {
+                System.Console.WriteLine(teamBasedOnId.Name);
+            }
+
+        }
+
+        public static void StampAllTeams()
+        {
+            // select all the teams
+            // SELECT * FROM Teams
             List<Team> teams = context.Teams.ToList();
 
             foreach (Team team in teams)
