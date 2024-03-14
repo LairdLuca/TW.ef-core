3701a0f86df6ab74ba94cd6663ee07e8f4a709c8
LairdLuca <luca.sanna.1503@gmail.com>
2024-03-14 02:10:53 +0100

    Adding One-To-One Relationships

--- src/EntityFrameworkCore.Domain/Team.cs
+++ src/EntityFrameworkCore.Domain/Team.cs
@@ -5,11 +5,12 @@
         public string Name { get; set; }
         
         public int CoachId { get; set; }
+        public Coach Coach { get; set; }
 
         public int? LeagueId { get; set; }
         public League? League { get; set; }
 
-        public List<Match> HomeMatches { get; set; }
-        public List<Match> AwayMatches { get; set; }
+        public List<Match> HomeMatches { get; set; } = new List<Match>();
+        public List<Match> AwayMatches { get; set; } = new List<Match>();
     }
 }
